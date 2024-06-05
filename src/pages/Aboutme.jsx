8fc2770d8ce2825aa299a5ed07102415b4c67bf8
Thomas Wolfe <tjwolfe91@gmail.com>
2024-06-05 17:17:04 -0400

    Update Aboutme.jsx

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -152,7 +152,7 @@ export default function Aboutme() {
           </p>
         </div>
         <div className="flex flex-col gap-2 justify-center md:justify-end w-full md:w-1/3 h-96 md:h-64 items-center">
-          <img src={Images?.Wolfe} alt="" className="" onClick={() => {}} />
+          <img src={Images?.Wolfe} alt="" height="100%" className="" onClick={() => {}} />
         </div>
       </div>
       {!showMore && (
