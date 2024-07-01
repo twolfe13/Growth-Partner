d2e71c056a66a4c077f48fb605579d02b593b539
Thomas Wolfe <tjwolfe91@gmail.com>
2024-07-01 15:19:20 -0400

    Update Aboutme.jsx

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -126,7 +126,7 @@ export default function Aboutme() {
           </p>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row  w-full px-10 justify-between py-[200px] gap-10 md:px-[100px]">
+      <div className="flex flex-col md:flex-row  w-full px-10 justify-between py-[140px] gap-10 md:px-[100px]">
         <div className="flex flex-col gap-6 w-full md:w-2/3">
           <h2 className=" text-4xl text-black font-primary-bd">
             My Career as a Salesperson -
@@ -151,7 +151,7 @@ export default function Aboutme() {
             team).
           </p>
         </div>
-        <div className="flex flex-col gap-2 justify-center md:justify-end w-full md:w-1/5 h-96 md:h-64 items-center">
+        <div className="flex flex-col gap-2 justify-center md:justify-end w-full md:w-1/6 h-96 md:h-64 items-center">
           <img src={Images?.Wolfe} alt="" className="" onClick={() => {}} />
         </div>
       </div>
